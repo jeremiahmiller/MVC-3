805fbd1104c78c7fbcedd81d6ec55993a5812880
Jeremiah Miller <j_miller429@yahoo.com>
2017-04-10 16:35:59 -0400

    added MVC

---
+++ GuessingGame/Models/GameModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame.Models
+{
+    public class GameModel
+    {
+        [Display(Name = "Enter your name")]
+        [Required(ErrorMessage = "You must enter your name")]
+        [MinLength(2, ErrorMessage = "Name is too short")]
+        [MaxLength(20, ErrorMessage = "Name is too long")]
+        public string PlayerName { get; set; }
+
+        [Display(Name = "What is your guess?")]
+        [Required(ErrorMessage = "Guess is required")]
+        [Range(1, 10, ErrorMessage = "Guess must be between 1 and 10")]
+        public int Guess { get; set; }
+    }
+}
